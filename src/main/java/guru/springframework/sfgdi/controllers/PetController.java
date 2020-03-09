b39ac9b2cfd8db72d0849aa72be958082d001e85
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-03-09 09:32:02 -0400

    update

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,21 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.data.ConditionalOnRepositoryType;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
-    private final PetService petService;
+    private PetService petService;
+
+    public PetController(@Qualifier("pet") PetService petService){
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
